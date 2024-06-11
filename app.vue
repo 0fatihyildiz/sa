f303de2e1c1afad6578a45cc251e0b473c4de99e
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-06-11 13:36:28 +0300

    ahmet delibas

--- app.vue
+++ app.vue
@@ -1,6 +1,3 @@
 <template>
-  <div class="bg-background">
-    <SButton size="lg">Primary</SButton>
-  </div>
   <NuxtPage />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,7 @@ const route = useRoute()
 </script>
 
 <template>
-  <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+  <div class="h-full w-full">
+    <SInput v-model="route.query.q" />
   </div>
 </template>
